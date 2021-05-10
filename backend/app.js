e6edd4afffad66d9e2f7e8dcb572b452c329082a
dat.phan <dat.phan@orientsoftware.com>
2021-05-10 16:32:14 +0700

    init soure backend

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+io.on('connection', socket => {
+    socket.on('room_join_request', payload => {
+        socket.join(payload.roomName, err => {
+            if (!err) {
+                io.in(payload.roomName).clients((err, clients) => {
+                    if (!err) {
+                        io.in(payload.roomName).emit('room_users', clients)
+                    }
+                });
+            }
+        })
+    })
+
+    socket.on('offer_signal', payload => {
+        io.to(payload.calleeId).emit('offer', { signalData: payload.signalData, callerId: payload.callerId });
+    });
+
+    socket.on('answer_signal', payload => {
+        io.to(payload.callerId).emit('answer', { signalData: payload.signalData, calleeId: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('room_left', { type: 'disconnected', socketId: socket.id })
+    })
+});
+
+http.listen(port, () => console.log('listening on *:' + port));
