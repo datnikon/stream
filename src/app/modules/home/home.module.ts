e02ef372693a8708887f19afc9c639e58a8b8fb7
dat.phan <dat.phan@orientsoftware.com>
2021-05-27 09:35:28 +0700

    Remove CommonModule and FormsModule in HomeModule

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,4 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 
@@ -13,7 +11,7 @@ const homeRoutes: Routes = [
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [RouterModule.forChild(homeRoutes), CommonModule, FormsModule],
+    imports: [RouterModule.forChild(homeRoutes)],
     exports: [RouterModule]
 })
 export class HomeModule {
